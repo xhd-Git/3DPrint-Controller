8a34bd43d51be986f482d6fc13f1ec5107f8254f
xhd-Git <baili_xx@163.com>
2017-04-05 21:03:21 +0800

    init_commit

---
+++ app/src/main/java/com/realwork/reol/a3dprint_controller/WelAct.java
@@ -0,0 +1,13 @@
+package com.realwork.reol.a3dprint_controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wel);
+    }
+}
