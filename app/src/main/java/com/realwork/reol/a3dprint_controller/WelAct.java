964eea2e5753198d80b229e01ccd1a23094796fe
xhd-Git <baili_xx@163.com>
2017-04-05 22:53:50 +0800

    the first commit

--- app/src/main/java/com/realwork/reol/a3dprint_controller/WelAct.java
+++ app/src/main/java/com/realwork/reol/a3dprint_controller/WelAct.java
@@ -1,13 +1,40 @@
 package com.realwork.reol.a3dprint_controller;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.realwork.reol.a3dprint_controller.ui.MainActivity;
+
 public class WelAct extends AppCompatActivity {
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            startActivity(new Intent(WelAct.this, MainActivity.class));
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wel);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    handler.sendEmptyMessage(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 }
