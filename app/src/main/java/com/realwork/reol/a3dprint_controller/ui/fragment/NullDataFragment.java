4b941fddc587fe07682083976fdd48cd637b411c
xhd-Git <baili_xx@163.com>
2017-04-16 22:54:14 +0800

    add apk file

---
+++ app/src/main/java/com/realwork/reol/a3dprint_controller/ui/fragment/NullDataFragment.java
@@ -0,0 +1,10 @@
+package com.realwork.reol.a3dprint_controller.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by reol on 16/04/2017.
+ */
+
+public class NullDataFragment extends Fragment {
+}
